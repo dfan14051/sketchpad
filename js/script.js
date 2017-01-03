08096239e40d91a7c818ac46a97c9a5780c37fdb
David Fan <dfan14051@gmail.com>
2017-01-02 19:15:07 -0500

    Cleaned up code

--- js/script.js
+++ js/script.js
@@ -1,35 +1,37 @@
-$(document).ready(function(){
-	makeTable(16);
+$(document).ready(function () {
+  makeTable(16);
 });
 
-function makeTable(sideLen){
-	var row = "<div class='row'></div>";
-	var $container = $(".container");
-	for(var i = 0; i < sideLen; i++){
-		$container.append(row);	
-	}
-	var $rows = $(".row");
-	for(var i = 0; i < sideLen; i++){
-		$rows.append("<div class='square'></div>");
-	}
-	var $squares = $(".square");
-	$squares.css("height", dimensionCalc(sideLen));
-	$squares.css("width", dimensionCalc(sideLen));
-	$squares.mouseenter(function(){
-		$(this).addClass("black");
-	});
+function makeTable(sideLen) {
+  var row = "<div class='row'></div>";
+  var $container = $('.container');
+  for (var i = 0; i < sideLen; i++) {
+    $container.append(row);
+  }
+
+  var $rows = $('.row');
+  for (var i = 0; i < sideLen; i++) {
+    $rows.append("<div class='square'></div>");
+  }
+
+  var $squares = $('.square');
+  $squares.css('height', dimensionCalc(sideLen));
+  $squares.css('width', dimensionCalc(sideLen));
+  $squares.mouseenter(function () {
+    $(this).addClass('black');
+  });
 }
 
-function dimensionCalc(sideLen){
-	return 550/(sideLen);
+function dimensionCalc(sideLen) {
+  return 550 / (sideLen);
 }
 
-function newTable(){
-	clearTable();
-	var tableSize = prompt("Please enter a new table size (default is 16)", "16");
-	makeTable(tableSize);
+function newTable() {
+  clearTable();
+  var tableSize = prompt('Please enter a new table size (default is 16)', '16');
+  makeTable(tableSize);
 }
 
-function clearTable(){
-	$(".container").empty();
+function clearTable() {
+  $('.container').empty();
 }
